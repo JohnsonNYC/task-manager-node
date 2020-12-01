e36721f486ff01688c71a1d643cca966cf8d8660
JohnsonNYC <jkow95@gmail.com>
2020-12-01 12:06:27 -0500

    Updated File structure and added more error handlers

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,21 +1,7 @@
-// Mongoose being used to build out models and connect to database 
+// Mongoose being used to build out models and connect to database .
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true ,
     useCreateIndex: true
-})
-
-
-
-const Task = mongoose.model('Task',{
-    description:{
-        type: String,
-        trim: true, 
-        required: true
-    },
-    completed:{
-        type: Boolean, 
-        default: false
-    }
 })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 const express = require('express')
 const app = express()
 const User = require('./models/users')
+const Task = require('./models/tasks')
+
 require('./db/mongoose')
 
 const port = process.env.PORT || 3000;
@@ -13,12 +15,22 @@ app.post('/users', (req,res)=>{
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
+        res.status(201).send(user)
+    }).catch((e)=>{
         res.status(400).send(e)
-    
     })
 })
+
+app.post('/tasks', (req,res)=>{
+    const task = new Task(req.body)
+
+    task.save().then(()=>{
+        res.status(201).send(task)
+    }).catch((error)=>{
+        res.status(400).send(error)
+    })
+})
+
 
 app.listen(port, () =>{
     console.log("Server is up on " + port)
