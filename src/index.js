ff0cd83bbc4a63c45a6107881eebd89cb97cfee7
JohnsonNYC <jkow95@gmail.com>
2020-12-02 13:01:05 -0500

    Created Reading endpoint for users

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json()) // parse the incoming JSON for us to have it accessbile as an object 
 
+// REST API for Creating Resources 
 app.post('/users', (req,res)=>{
     const user = new User(req.body)
 
@@ -18,6 +19,29 @@ app.post('/users', (req,res)=>{
         res.status(201).send(user)
     }).catch((e)=>{
         res.status(400).send(e)
+    })
+})
+// REST API for Reading Resources  
+    // Index - show all users 
+app.get('/users',(req,res)=>{
+    User.find({}).then((Users)=>{
+        res.send(Users)
+    }).catch((e)=>{
+        res.status(500).send()
+    }); // returns all users
+})
+    //Show - show one user
+
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(500).send()
     })
 })
 
